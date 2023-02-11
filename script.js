af5c3060349ce56742332a1e3cfe9d096e0fe135
Anwesha-ghoshcse <111915637+Anwesha-ghoshcse@users.noreply.github.com>
2023-02-11 16:20:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+console.log("i am running");
+let string = "";
+let buttons = document.querySelectorAll('.btn');
+setTimeout(() => {
+    Array.from(buttons).forEach((btn) => {
+        btn.addEventListener('click', (e) => {
+            if (e.target.innerHTML == '=') {
+                string = eval(string);
+                document.querySelector('input').value = string;
+
+            }
+            else if (e.target.innerHTML == 'x') {
+                e.target.innerHTML = '*';
+                string += e.target.innerHTML;
+                //string = eval(string);
+                document.querySelector('input').value = string;
+            }
+            else if (e.target.innerHTML == 'C') {
+                string = "";
+                document.querySelector('input').value = string;
+            }
+            else if (e.target.innerHTML == 'DL') {
+                string = "";
+                document.querySelector('input').value = string;
+            }
+            else {
+                console.log(e.target)
+                string = string + e.target.innerHTML;
+                document.querySelector('input').value = string;
+
+            }
+
+        })
+    })
+},1000)
